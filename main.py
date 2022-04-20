5bebb00f4d07a45f0370e13a9772d6a013a328a5
manarashraf2 <92338031+manarashraf2@users.noreply.github.com>
2022-04-20 17:46:00 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,47 +1,38 @@
 import pandas as pd
-import seaborn as sb
+#import seaborn as sb
 import matplotlib.pyplot as plt
-from sklearn.model_selection import KFold
+#from sklearn.model_selection import KFold
 
-HD=pd.read_csv("/Users/nadahmed/Downloads/Semester 6/Data Mining/project/heart.csv")
-#HD.info();
-#check for duplicates before and after removing them
-#var = HD[HD.duplicated()]
-#print(var.shape)
+HD=pd.read_csv(r"C:\Users\manar\PycharmProjects\heartdiseaseprediction\heart.csv")
+#HD.info()
+
+#check for duplicates and removing them
+#HD = HD[HD.duplicated()]
+#print(HD.shape)
 HD=HD.drop_duplicates()
-#var = HD[HD.duplicated()]
-#print(var.shape)
+#HD = HD[HD.duplicated()]
+#print(HD.shape)
 
 #check for null values
-#print(HD.isnull())
+#print(HD.isnull().sum())
 
 #check for outliers
-#method1
-#plt.boxplot(HD["fbs"])
+#plt.boxplot(HD.drop('target', axis=1))
 #plt.show()
 
-#method2
-#print("Highest_value",HD['ca'].mean() + 3*HD['ca'].std())
-#print("Lowest_value",HD['ca'].mean() - 3*HD['ca'].std())
-#hd = HD[(HD['ca'] < -2.301701729570903) | (HD['ca'] > 3.7387878222861346)]
-#print(hd)
-
-#remove outliers 
+#remove outliers
 Q1 = HD.quantile(0.25)
 Q3 = HD.quantile(0.75)
 IQR = Q3-Q1
-data2 = HD[~((HD<(Q1-1.5*IQR))|(HD>(Q3+1.5*IQR))).any(axis=1)]
-print(data2.shape)
+HD = HD[~((HD<(Q1-1.5*IQR))|(HD>(Q3+1.5*IQR))).any(axis=1)]
 
-#sampling data by stratified sampling
-ds=pd.DataFrame(HD)
-print(ds)
-dsgroup=ds.groupby('age', group_keys=False)
-dssample=dsgroup.apply(lambda x: x.sample(frac=0.6))
-print(dssample)
+#dataframe of dataset
+HD=pd.DataFrame(HD)
+print(HD.shape)
 
-                       
-
-
-
-
+#shuffling and sampling data by stratified sampling
+HD=HD.sample(frac=1)
+dsgroup=HD.groupby('age', group_keys=False)
+dssample=dsgroup.apply(lambda x: x.sample(frac=0.55))
+#print(dssample)
+print(dssample.shape)
